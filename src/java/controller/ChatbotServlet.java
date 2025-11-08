f7b8d6e024780351506cb8d8955f46e441b21729
Kervi Falcón <isaifalcon10m@gmail.com>
2025-11-07 22:12:58 -0500

    Correccion de conector

--- src/java/controller/ChatbotServlet.java
+++ src/java/controller/ChatbotServlet.java
@@ -21,7 +21,7 @@ import java.util.*;
 @WebServlet("/chatbot")
 public class ChatbotServlet extends HttpServlet {
     
-    private static final String GEMINI_API_KEY = "AIzaSyCGn5-7Jzj83RMDhLerVWg_zOJl3f8TfPo";
+    private static final String GEMINI_API_KEY = "";
     private static final String GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
     
     private ChatbotDAO chatbotDAO;
